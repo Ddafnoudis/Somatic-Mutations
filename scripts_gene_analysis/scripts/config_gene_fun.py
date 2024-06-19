c2e55a3b7450d91571b656382de843815d8e68ef
Dimitris Dafnoudis <138138270+Ddafnoudis@users.noreply.github.com>
2024-06-19 07:59:18 +0200

    Update config_gene_fun.py

--- scripts_gene_analysis/scripts/config_gene_fun.py
+++ scripts_gene_analysis/scripts/config_gene_fun.py
@@ -21,16 +21,6 @@ def parse_configuration_files(fname) -> Dict[int, str]:
     config['GENE_FILES_FOLDER'] = Path(config['GENE_FILES_FOLDER'])
     config['HALLMARK_RESULTS'] = Path(config['HALLMARK_RESULTS'])
     config['CLL_ENR_RESULTS'] = Path(config['CLL_ENR_RESULTS'])
-    # config['CORR_IMAGE'] = Path(config['CORR_IMAGE'])
-    # config['CORR_RESULTS'] = Path(config['CORR_RESULTS'])
-    # config['LZP_RESULTS'] = Path(config['LZP_RESULTS'])
-    # config['ANOVA'] = Path(config["ANOVA"])
-    # config['MIT'] = Path(config["MIT"])
-    # config['CROSS_VAL'] = Path(config['CROSS_VAL'])
-    # config['ACCURACY'] = Path(config['ACCURACY'])
-    # config['REPORT_RF'] = Path(config['REPORT_RF'])
-    # config['CONFUSION_MTX'] = Path(config['CONFUSION_MTX'])
-    # config["MLP_RESULTS"] = Path(config["MLP_RESULTS"])
 
     return config
 
