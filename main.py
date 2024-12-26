5e6101efff4e324136b713aaa214a10ec3916d58
Dimitris Dafnoudis <dimitrisdaf@hotmail.com>
2024-12-26 11:44:28 +0100

    Configuration parameters added

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from scripts.condition_statements import condition_statement
 def main():
     config = parse_configuration_files(fname='configuration.yaml')
 
-    condition_statement(seed=config["SEED"], working_dir=config["WORK_DIR"], 
+    condition_statement(seed=config["SEED"], epochs=config["EPOCHS"], 
+                        working_dir=config["WORK_DIR"], 
                         data=config["DATASET"], output_dir = config["OUTPUT_DIR"],
                         corr_image =config["CORR_IMAGE"], corr_results =config['CORR_RESULTS'],
-                        lzp_results = config['LZP_RESULTS'], anova = config["ANOVA"], 
-                        mit = config["MIT"], cross_val = config['CROSS_VAL'], 
+                        lzp_results = config['LZP_RESULTS'], 
+                        cross_val = config['CROSS_VAL'], 
                         accuracy = config['ACCURACY'], report_rf = config["REPORT_RF"],
-                        confusion_mtx = config["CONFUSION_MTX"], mlp_results=config["MLP_RESULTS"])
+                        confusion_mtx = config["CONFUSION_MTX"], mlp_results=config["MLP_RESULTS"],
+                        param_grid=config["PARAM_GRID"])
 
 
 if __name__ == "__main__":
