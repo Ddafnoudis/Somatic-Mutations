b39e4a10a757e4c5198de4818fc58d4acd1a2d07
Dimitris Dafnoudis <dimitrisdaf@hotmail.com>
2025-01-04 13:04:43 +0100

    RF parameters configuration

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ def main():
                         working_dir=config["WORK_DIR"], 
                         data=config["DATASET"], output_dir = config["OUTPUT_DIR"],
                         corr_image =config["CORR_IMAGE"], corr_results =config['CORR_RESULTS'],
+                        n_optimal_trees = config['N_OPTIMAL_TREES'],
                         lzp_results = config['LZP_RESULTS'], 
                         cross_val = config['CROSS_VAL'], 
                         accuracy = config['ACCURACY'], report_rf = config["REPORT_RF"],
                         confusion_mtx = config["CONFUSION_MTX"], mlp_results=config["MLP_RESULTS"],
+                        best_params = config["BEST_PARAMS"], rf_parameters=config["RF_PARAMS"],
                         epochs=config["EPOCHS"], param_grid=config["PARAM_GRID"])
 
 
