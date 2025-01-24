10c9472d25b693272b820042848f7b5d46572cd7
Dimitris Dafnoudis <dimitrisdaf@hotmail.com>
2025-01-24 12:37:42 +0100

    Needs configuration

--- scripts_gene_analysis/scripts/enrich_gene.py
+++ scripts_gene_analysis/scripts/enrich_gene.py
@@ -4,10 +4,11 @@ Perform over-representation analysis using hallmark gene set files.
 import pandas as pd
 import gseapy as gp
 import seaborn as sns
+from pathlib import Path
 import matplotlib.pyplot as plt 
 
 
-def over_representation_analysis():
+def over_representation_analysis(gene_file_folder: Path, enr_res_folder: Path):
     """
     Perform over-representation analysis using hallmark gene set files.
     """
@@ -18,11 +19,11 @@ def over_representation_analysis():
     # Iterate through each gene list file and its corresponding title
     for file, title_name in zip(files, title_names):
         # Read the gene list from the file
-        df = pd.read_csv(f'gene_folder/{file}', sep='\t', dtype=object)
+        df = pd.read_csv(f'{gene_file_folder}/{file}', sep='\t', dtype=object)
         
         # Print available gene set libraries
         names = gp.get_library_name(organism="Human")
-        print(f"Names: \n{names[:10]}\n")
+        print(f"Names: \n{names[:5]}\n")
         
         # Convert dataframe to list
         glist = df.squeeze().str.strip().to_list()
@@ -34,7 +35,7 @@ def over_representation_analysis():
         # Perform over-representation analysis
         enr_over_repr = gp.enrichr(gene_list=glist,
                                 organism="human",
-                                gene_sets=["Reactome 2022"],
+                                gene_sets=["Reactome_2022"],
                                 cutoff=significance_threshold,
                                 verbose=True)
         
@@ -42,7 +43,7 @@ def over_representation_analysis():
         enr_results = enr_over_repr.results
 
         # Save the enrichment results to a file
-        enr_results.to_csv(f"hallmark/{file}_{set}enrichment_results.csv", sep='\t', index=False)
+        enr_results.to_csv(f"{enr_res_folder}/{title_name}enrichment_results.csv", sep='\t', index=False)
         
         # Filter significant pathways
         significant_pathways = enr_results[enr_results['Adjusted P-value'] < significance_threshold]
@@ -69,7 +70,7 @@ def over_representation_analysis():
             plt.xlabel('Gene Set')
             plt.ylabel('Adjusted P-value')
             plt.tight_layout()
-            plt.savefig(f"hallmark/dotplot_{title_name}.png")
+            plt.savefig(f"{enr_res_folder}dotplot_{title_name}.png")
             plt.close()
 
     return enr_results
--- scripts_gene_analysis/scripts/gene_list.py
+++ scripts_gene_analysis/scripts/gene_list.py
@@ -1,29 +1,36 @@
 """
 Generate lists of genes out of the dataset
 """
+from typing import List
+from pathlib import Path
+from pandas import DataFrame
 
 
-def gene_list_(dataset):
+def gene_list_(dataset: DataFrame, gene_file_folder: Path)-> List[str]:
     """
     Generate 4 files with the lists of genes
     1) The first should contain all the genes from the dataset
     2) The rest should contain the genes for each cancer type
     """
+    # Define the gene list
     gene_list = dataset["Hugo_Symbol"]
-    gene_list.to_csv('gene_folder/gene_list.txt', index=False)
 
+    # Define the abbreviation of the three leukemias
     all_cancer_type = "ALL"
     laml_cancer_type = "LAML"
     cll_cancer_type = "CLL"
 
+    # Locate the ALL genes in the dataset and save them in separate file
     all_selected_genes = dataset.loc[dataset['Disease_Type'] == all_cancer_type, 'Hugo_Symbol']
-    all_selected_genes.to_csv("gene_folder/ALL_gene_list.txt", index=False, header=False)
-
+    all_selected_genes.to_csv(gene_file_folder / "ALL_gene_list.txt", index=False, header=False)
+    
+    # Locate the AML genes in the dataset and save them in separate file
     laml_selected_genes = dataset.loc[dataset['Disease_Type'] == laml_cancer_type, 'Hugo_Symbol']
-    laml_selected_genes.to_csv("gene_folder/LAML_gene_list.txt", index=False, header=False)
-
+    laml_selected_genes.to_csv(gene_file_folder / "LAML_gene_list.txt", index=False, header=False)
+    
+    # Locate the CLL genes in the dataset and save them in separate file
     cll_selected_genes = dataset.loc[dataset['Disease_Type'] == cll_cancer_type, 'Hugo_Symbol']
-    cll_selected_genes.to_csv("gene_folder/CLL_gene_list.txt", index=False, header=False)
+    cll_selected_genes.to_csv(gene_file_folder / "CLL_gene_list.txt", index=False, header=False)
     
     return gene_list, all_selected_genes, laml_selected_genes, cll_selected_genes
 
