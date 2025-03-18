0a78748cb8de7e71314abb45c6c856aa114f1990
Dimitris Dafnoudis <dimitrisdaf@hotmail.com>
2025-03-18 13:01:59 +0100

    define features and target

--- scripts/target_features.py
+++ scripts/target_features.py
@@ -9,9 +9,14 @@ def full_dataframe(data):
     """
     # Read the data
     data = pd.read_csv(data, sep="\t", dtype=object)
-    # Define features and target
+    # Define features
     features = data.iloc[:, :-1]
+    # Print features
+    print(f"Features shape and features: \n{features.shape}\n{features}")
+    # Define the target
     target = data["Disease_Type"]
+    print(f"Target shape and target: \n{target.shape}\n{target}")
+    # Define the target classes
     target_classes = target.unique().tolist()
 
     return features, target, target_classes
