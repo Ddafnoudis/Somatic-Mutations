c064cb1bddc6f0c27fe16c7b1d0aa8c4045b6414
Dimitris Dafnoudis <dimitrisdaf@hotmail.com>
2025-03-18 13:00:15 +0100

    anova, chi and cramers V

--- scripts/correlation.py
+++ scripts/correlation.py
@@ -6,147 +6,131 @@ Returns:
     - Cramers V correlation tsv file
     - Categorical correlation heatmap
 """
-import os
-import numpy as np
 import pandas as pd
+import numpy as np
 import seaborn as sns
-from tqdm import tqdm
-import scipy.stats as ss
 from pandas import DataFrame
 import matplotlib.pyplot as plt
-from scipy.stats import spearmanr
+from scipy.stats import chi2_contingency
+from sklearn.feature_selection import chi2, f_classif
 
 
-def correlation(categorical_dataset: DataFrame, numerical_dataset: DataFrame, significant_threshold: float):
+def correlation(categorical_dataset: DataFrame, 
+                numerical_dataset: DataFrame,
+                target: DataFrame):
     """
     Compute the correlation between categorical and numerical features.
-    - Spearman correlation: numerical datasets
+    - ANOVA F-value: numerical dataset
     - Cramér's V statistic: categorical dataset
     """
 
-    def spearman_correlation(numerical_dataset: DataFrame):
+    def anova_correlation(numerical_dataset, target):
         """
-        Calculates the Spearman correlation matrix.
+        Calculates the ANOVA F-value.
         """
-        # Define the dictionary 
-        spearman_dict = {
-            "feature1": [],
-            "feature2": [],
-            "spearman": [],
-            "p_value": []
-        }
+        # Compute the ANOVA F-value and p-value
+        anova_scores, p_values = f_classif(numerical_dataset, target)
 
-        for first_feature in numerical_dataset:
-            for second_feature in numerical_dataset:
-                # if second_feature.name != first_feature:
-                rho, p = spearmanr(numerical_dataset[first_feature], 
-                                   numerical_dataset[second_feature])
-                if p < significant_threshold:
-                    spearman_dict["feature1"].append(first_feature)
-                    spearman_dict["feature2"].append(second_feature)
-                    spearman_dict["spearman"].append(rho)
-                    spearman_dict["p_value"].append(p)
-
-        # Spearman correlation to DatFrame
-        spearman_file = pd.DataFrame(spearman_dict, columns=["feature1", "feature2", "spearman", "p_value"])
-        if not os.path.exists("result_files/correlation_folder"):
-            os.makedirs("result_files/correlation_folder", exist_ok=True)
-        # Save the results to a TSV file
-        spearman_file.to_csv("result_files/correlation_folder/spearman_correlation.tsv", "\t")
+        # Calculate degrees of freedom
+        n_groups = len(target)  # 3 leukemia types
+        n_samples = len(numerical_dataset)
+        df_between = n_groups - 1
+        df_within = n_samples - n_groups
         
-        return spearman_file
+        # Calculate eta-squared
+        eta_sq = (anova_scores * df_between) / (anova_scores * df_between + df_within)
+    
+        # Create a dataframe with the ANOVA F-value and p-value
+        anova_results = pd.DataFrame({
+            "Feature": numerical_dataset.columns,
+            "ANOVA F-Score": anova_scores,
+            "p-value": p_values,
+            "eta-squared": eta_sq
+        })
 
-    def cramers_v(confusion_matrix: DataFrame):
-        """
-        Calculates the Cramér's V statistic for a given confusion matrix.
-        """
-        chi2 = ss.chi2_contingency(confusion_matrix)[0]
+    
+        # Save the results to a tsv file
+        anova_results.to_csv("result_files/correlation_folder/anova_correlation.tsv", sep="\t", index=False)
+        
+        # Return the results
+        return anova_results
+
+    def cramers_v(x, y):
+        confusion_matrix = pd.crosstab(x, y)
+        chi2_stat, _, _, _ = chi2_contingency(confusion_matrix)
         n = confusion_matrix.sum().sum()
-        phi2 = chi2 / n
+        phi2 = chi2_stat / n
         r, k = confusion_matrix.shape
-        phi2corr = max(0, phi2 - ((k - 1) * (r - 1)) / (n - 1))
-        rcorr = r - ((r - 1) ** 2) / (n - 1)
-        kcorr = k - ((k - 1) ** 2) / (n - 1)
+        cramers_v = np.sqrt(phi2 / min(k-1, r-1))
+
+        return cramers_v
+    
+    def chi_square_correlation(categorical_dataset: DataFrame, target: DataFrame):
+        """
+        Calculates the Chi-Square statistic for categorical features.
+        """
+        # Compute Cramer's V for each categorical feature
+        effect_sizes = [cramers_v(categorical_dataset[col], target) for col in categorical_dataset.columns]
+        # Compute the Chi-Square score and p-value
+        chi_scores, p_values = chi2(pd.get_dummies(categorical_dataset), target)
         
-        return np.sqrt(phi2corr / min((kcorr - 1), (rcorr - 1)))
-    
-    
-    def categorical_correlation(categorical_dataset: DataFrame):
-        """
-        Calculates the categorical correlation matrix.
-        The function iterates through each pair of columns in the DataFrame, 
-        calculates the confusion matrix, and then computes Cramér's V statistic 
-        to measure the association between the two categorical variables.
-        """
-        # Define the correlation matrix columns
-        categorical_corr = DataFrame(index=categorical_dataset.columns, columns=categorical_dataset.columns)
-        with tqdm(total=1000, desc="Computing Correlation", dynamic_ncols=True) as pbar:
-            for first_feature in categorical_dataset.columns:
-                for second_feature in categorical_dataset.columns:
-                    # print(f"First feature: {i} \n Second feature: {j}\n\n")
-                    confusion_matrix = pd.crosstab(categorical_dataset[first_feature], categorical_dataset[second_feature])
-                    # print(f"Confusion Matrix: \n{confusion_matrix}\n\n")
-                    categorical_corr.loc[first_feature, second_feature] = cramers_v(confusion_matrix)
-            pbar.update(1000)        
+        # Create a dataframe with the Chi-Square score and p-value
+        chi_results = pd.DataFrame({
+            "Feature": categorical_dataset.columns,
+            "Chi-Square Score": chi_scores,
+            "p-value": p_values,
+            "cramers-v": effect_sizes
+        })
 
-        return categorical_corr
+        # Save the results to a tsv file
+        chi_results.to_csv("result_files/correlation_folder/chi_square_correlation.tsv", sep="\t", index=False)
+        
+        # Return the results
+        return chi_results
     
-    # Assuming `features` is a DataFrame containing only categorical features
-    categorical_corr_matrix = categorical_correlation(categorical_dataset)
 
-
-    def plot_categorical_correlation(corr_matrix):
+    def plot_categorical_correlation(chi_results):
         """
-        Plots the categorical correlation matrix.
+        Plots the Chi-Square correlation matrix with a publication-ready format.
         """
-        plt.figure(figsize=(12, 10))
-        sns.heatmap(corr_matrix.astype(float), cmap='coolwarm', linewidths=0.5)
-        for i in range(len(corr_matrix)):
-            for j in range(len(corr_matrix.columns)):
-                plt.text(j+0.5, i+0.5, '{:.2f}'.format(corr_matrix.iloc[i, j]), ha='center', va='center', fontsize=10)
-        plt.title('Categorical Correlation Matrix')
-        plt.xlabel('Features')
-        plt.ylabel('Features')
-        plt.savefig('result_files/correlation_folder/categorical_correlation.png')
+        # Figure size
+        plt.figure(figsize=(12, 6))
+        # Violin plot
+        sns.barplot(data=chi_results, y="Chi-Square Score", x="Feature", palette="magma")
+        
+        plt.title('Chi-Square Correlation Matrix', fontsize=14, fontweight='bold')
+        plt.xlabel('Feature', fontsize=12)
+        plt.ylabel('Chi-Square Correlation', fontsize=12)
+        plt.xticks(rotation=45, ha='right', fontsize=10)
+        plt.yticks(fontsize=10)
+        plt.tight_layout()
+        plt.savefig('result_files/correlation_folder/categorical_correlation.png', dpi=300)
         # plt.show()
 
-    plot_categorical_correlation(categorical_corr_matrix)
-    
-    # Define the results dictionary
-    cramers_v_dict = {
-        "feature1": [],
-        "feature2": [],
-        "correlation": [],
-        "p_value": []
-    }
+    def plot_numerical_correlation(anova_results):
+        """
+        Plots the ANOVA correlation matrix with a publication-ready format.
+        """
+        plt.figure(figsize=(6, 6))
+        sorted_results = anova_results.sort_values(by="ANOVA F-Score", ascending=False)
 
-    # Iterate through each pair of features and calculate the p-value
-    with tqdm(total=1000, desc="Computing Correlation", dynamic_ncols=True) as pbar:
-        for first_feature in range(len(categorical_corr_matrix)):
-            for second_feature in range(first_feature+1, len(categorical_corr_matrix.columns)):
-                # Define the correlation number 
-                corr = float(categorical_corr_matrix.iloc[first_feature, second_feature])
-                # Define the p-value
-                p_value = ss.chi2_contingency(pd.crosstab(categorical_dataset.iloc[:, first_feature], categorical_dataset.iloc[:, second_feature]))[1]
-                # Set condition
-                if p_value < significant_threshold:
-                    if corr < 0.25:
-                        # Append the results to the dictionary's lists
-                        cramers_v_dict["feature1"].append(categorical_corr_matrix.columns[first_feature])
-                        cramers_v_dict["feature2"].append(categorical_corr_matrix.columns[second_feature])
-                        cramers_v_dict["correlation"].append(corr)
-                        cramers_v_dict["p_value"].append(p_value)
-        pbar.update(1000)
+        sns.barplot(data=sorted_results, y="ANOVA F-Score", x="Feature", palette="magma")
+        
+        plt.title('ANOVA Correlation Matrix', fontsize=14, fontweight='bold')
+        plt.xlabel('Feature', fontsize=12)
+        plt.ylabel('ANOVA F-Score', fontsize=12)
+        plt.xticks(rotation=45, ha='right', fontsize=10)
+        plt.yticks(fontsize=10)
+        plt.tight_layout()
+        plt.savefig('result_files/correlation_folder/numerical_correlation.png', dpi=300)
+        # plt.show()
 
-    # Numerical dataset to DataFrame    
-    cramers_file = pd.DataFrame(cramers_v_dict, columns=["feature1", "feature2", "correlation", "p_value"])
-    # Save the results to a TSV file
-    cramers_file.to_csv("result_files/correlation_folder/correlation_results.tsv", "\t")
-    
-    # Execute and define the spearman function
-    spearman_file = spearman_correlation(numerical_dataset)
-    
-    return cramers_file, spearman_file
+    anova_results = anova_correlation(numerical_dataset, target)
+    chi_results = chi_square_correlation(categorical_dataset, target)
+    plot_categorical_correlation(chi_results)
+    plot_numerical_correlation(anova_results)
+
+    return anova_results, chi_results
 
 
 if __name__ == "__main__":
